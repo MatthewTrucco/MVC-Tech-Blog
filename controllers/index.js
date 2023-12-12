061b90de749dcebbfaa9ed87dbeac1be7c0c4ea8
MatthewTrucco <87626582+MatthewTrucco@users.noreply.github.com>
2023-12-11 19:27:55 -0500

    -

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
+
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
-const dashboardRoutes = require('./dashboard-routes');
+const dashboardRoutes = require('./dashboard-routes'); 
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
